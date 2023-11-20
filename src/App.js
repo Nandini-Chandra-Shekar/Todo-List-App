eb60c82b35c63bf39049694c98e5feb0f3e25ada
Nandini-Chandra-Shekar <151377279+Nandini-Chandra-Shekar@users.noreply.github.com>
2023-11-20 10:56:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import { TodoWrapper } from './Components/TodoWrapper';
+import { Helmet } from 'react-helmet';
+
+const TITLE = 'Todo List App';
 
 function App() {
   return (
+    <>
+    <Helmet>
+          <title>{ TITLE }</title>
+        </Helmet>
     <div className="App">
       <TodoWrapper  />
     </div>
+    </>
   );
 }
 
